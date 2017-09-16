8c0c1fd5c16e109b736f0b7226033f1e8a53852a
Matt Davis <matt_davis@me.com>
2017-09-16 17:03:33 -0500

    new model, car lap successful

--- model.py
+++ model.py
@@ -12,9 +12,10 @@ from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
 
-#parameters
+# global parameters
 TEST_FOLDER = '/data'
 EPOCHS = 3
+CORRECTION_FACTOR = 0.2
 
 def generator(samples, batch_size=32):
     num_samples = len(samples)
@@ -23,20 +24,20 @@ def generator(samples, batch_size=32):
         for offset in range(0, num_samples, batch_size):
             batch_samples = samples[offset:offset+batch_size]
 
-            images = []
-            angles = []
+            train_images = []
+            train_angles = []
             for batch_sample in batch_samples:
-                name = '.{}/IMG/{}'.format(TEST_FOLDER, batch_sample[0].split('\\')[-1])
-                center_image = cv2.imread(name)
-                center_angle = float(batch_sample[3])
-                images.append(center_image)
-                angles.append(center_angle)
-                images.append(cv2.flip(center_image, 1))
-                angles.append(center_angle * -1.0)
+                names = ['.{}/IMG/{}'.format(TEST_FOLDER, batch_sample[i].split('\\')[-1]) for i in range(3)]
+                images = [cv2.imread(name) for name in names]
+                angles = float(batch_sample[3]), float(batch_sample[3]) + CORRECTION_FACTOR, float(batch_sample[3]) - CORRECTION_FACTOR
+                train_images.extend(images)
+                train_angles.extend(angles)
+                train_images.extend([cv2.flip(img, 1) for img in images])
+                train_angles.extend([ang * -1.0 for ang in angles])
 
             # trim image to only see section with road
-            X_train = np.array(images)
-            y_train = np.array(angles)
+            X_train = np.array(train_images)
+            y_train = np.array(train_angles)
             yield shuffle(X_train, y_train)
 
 
@@ -59,18 +60,20 @@ validation_generator = generator(validation_samples, batch_size=32)
 model = Sequential()
 
 model.add(Lambda(lambda x: x/127.5 - 1., input_shape=(160, 320, 3)))  # Preprocess incoming data, centered around zero with small standard deviation
-model.add(Cropping2D(cropping=((50,20), (0,0))))
-model.add(Convolution2D(6, 3, 3, activation='relu'))
-model.add(MaxPooling2D())
-model.add(Convolution2D(6, 3, 3, activation='relu'))
-model.add(MaxPooling2D())
+model.add(Cropping2D(cropping=((70,25), (0,0))))
+model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(48, 5, 5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit_generator(train_generator, samples_per_epoch=len(train_samples)*2, validation_data=validation_generator, nb_val_samples=len(validation_samples)*2, nb_epoch=EPOCHS)
+model.fit_generator(train_generator, samples_per_epoch=len(train_samples)*6, validation_data=validation_generator, nb_val_samples=len(validation_samples)*6, nb_epoch=EPOCHS)
 print('Training complete!')
 
 model.save('model.h5')
